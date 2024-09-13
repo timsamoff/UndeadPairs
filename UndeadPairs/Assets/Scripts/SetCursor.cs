ff49c453c1ce9ad7c12a58285f244c40de6fa12b
Tim Samoff <tim@samoff.com>
2024-09-13 14:17:10 -0700

    Added back button and custom cursor

---
+++ UndeadPairs/Assets/Scripts/SetCursor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCursor : MonoBehaviour
+{
+
+    [SerializeField] private Texture2D cursorTexture;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorTexture, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
