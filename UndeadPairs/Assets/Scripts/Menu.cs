56ab931d4f338918471ada72a8a2cc518e891d8d
Tim Samoff <tim@samoff.com>
2024-09-17 12:36:01 -0700

    Refining UI elements, including the main menu

--- UndeadPairs/Assets/Scripts/Menu.cs
+++ UndeadPairs/Assets/Scripts/Menu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public void PracticeStart()
+    {
+        SceneManager.LoadScene("Practice");
+    }
+
     public void EasyStart()
     {
         SceneManager.LoadScene("Easy");
