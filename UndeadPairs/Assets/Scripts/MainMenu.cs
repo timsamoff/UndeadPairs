ccdb733fc5b6afc86102a33ddd9587c9fb9d7d21
Tim Samoff <tim@samoff.com>
2024-09-18 15:40:07 -0700

    Added hover/click sounds to Main Menu buttons

--- UndeadPairs/Assets/Scripts/MainMenu.cs
+++ UndeadPairs/Assets/Scripts/MainMenu.cs
@@ -5,23 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Interactions")]
+    [SerializeField] private AudioClip click;
+    [SerializeField] private AudioClip hover;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void PracticeStart()
     {
+        PlayClick();
         SceneManager.LoadScene("Practice");
     }
 
     public void EasyStart()
     {
+        PlayClick();
         SceneManager.LoadScene("Easy");
     }
 
     public void NormalStart()
     {
+        PlayClick();
         SceneManager.LoadScene("Normal");
     }
 
-    public void HardStart()
+    public void DifficultStart()
     {
+        PlayClick();
         SceneManager.LoadScene("Hard");
     }
 
@@ -29,4 +43,14 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     } */
+
+    private void PlayClick()
+    {
+        audioSource.PlayOneShot(click);
+    }
+
+    public void PlayHover()
+    {
+        audioSource.PlayOneShot(hover);
+    }
 }
